61d9da0c40e10f523f659fb3ea8bddd6fa7110b2
lkSander <lauracuestatorres@gmail.com>
2024-10-03 19:21:01 +0200

    3/10/24

--- Assets/Scrips/Enemies/Enemigo.cs
+++ Assets/Scrips/Enemies/Enemigo.cs
@@ -8,7 +8,7 @@ public class Enemigo : MonoBehaviour
     [SerializeField] float velocity;
     [SerializeField] Vector3 movimiento;
     float timer = 0f;
-    bool derecha=false;
+   
    
     void Start()
     {
@@ -24,34 +24,32 @@ public class Enemigo : MonoBehaviour
     void Movimiento()
     {
         timer += Time.deltaTime;
+        transform.Translate(movimiento.normalized * velocity * Time.deltaTime);
 
-        if (derecha == false)
-        {
-            transform.Translate(movimiento.normalized * velocity * Time.deltaTime);
+  
+        
             if (timer >= 2)
             {
-               
-                transform.Translate(movimiento.normalized * -1 * velocity * Time.deltaTime);
-                transform.eulerAngles = new Vector3(0, 90, 0);
-                derecha = true;
+                movimiento = movimiento * -1;
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                
                 timer = 0;
                 
               
             }
-        } 
+        
 
-        if (derecha == true)
-        {
-            transform.Translate(movimiento.normalized * 1 * velocity * Time.deltaTime);
+            
             if (timer >= 2)
             {
-                transform.Translate(movimiento.normalized * velocity * Time.deltaTime);
+               movimiento = movimiento * -1;
+           
                 transform.eulerAngles = new Vector3(0, 0, 0);
-                derecha = false;
+               
                 timer = 0;
             }
 
-        }
+        
 
        
         Debug.Log(timer);
--- Assets/Scrips/Personaje/Personaje.cs
+++ Assets/Scrips/Personaje/Personaje.cs
@@ -24,33 +24,44 @@ public class Personaje : MonoBehaviour
 
     {
 
+        float h= Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+
+
 
         //float z = 0;
         //float x = 0;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            //z++;
-            transform.position += new Vector3(0, 0, 1).normalized * velocity*Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A)&& Input.GetKey(KeyCode.W))
-        {
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    //z++;
+        //    transform.position += new Vector3(0, 0, 1).normalized * velocity*Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.A)&& Input.GetKey(KeyCode.W))
+        //{
             
-            transform.position += new Vector3(-1, 0, 1).normalized * velocity*Time.deltaTime;
+        //    transform.position += new Vector3(-1, 0, 1).normalized * velocity*Time.deltaTime;
            
-        }
-        //    x--;
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += new Vector3(0, 0, -1).normalized * velocity * Time.deltaTime;
-            //    z--;
-        }
-        if (Input.GetKey(KeyCode.D)&& Input.GetKey(KeyCode.W))
-        {
-            transform.position += new Vector3(1, 0, 1).normalized * velocity* Time.deltaTime;
-            //    x++;
-        }
-        //rb.velocity = new Vector3(x * velocity, 0, z * velocity);
+        //}
+        ////    x--;
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.position += new Vector3(0, 0, -1).normalized * velocity * Time.deltaTime;
+        //    //    z--;
+        //}
+        //if (Input.GetKey(KeyCode.D)&& Input.GetKey(KeyCode.W))
+        //{
+        //    transform.position += new Vector3(1, 0, 1).normalized * velocity* Time.deltaTime;
+        //    //    x++;
+        //}
+        ////rb.velocity = new Vector3(x * velocity, 0, z * velocity);
+
+        //if(Input.GetKey(KeyCode.D))
+        //{
+        //    transform.eulerAngles= new Vector3 (0,90,0);
+        //}
+
        
         
     }
